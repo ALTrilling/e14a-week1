c5493e5c50fe30e1ac28a617f70f9a4237553c61
adrian <adrian.trilling@gmail.com>
2022-09-25 21:10:02 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import datetime
+import time
+import pytz
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+@app.route('/timezones')
+def timezones():
+    current_time = time.strftime("%H:%M:%S")
+    berline_time = datetime.datetime.now(pytz.timezone('Europe/Berlin')).strftime("%H:%M:%S")
+    return 'Current time: ' + current_time + ', Berlin time: ' + berline_time
+
+@app.route('/hello/<MYNAME>')
+def hello(MYNAME):
+    return 'Hello ' + MYNAME
+
+if __name__ == '__main__':
+    app.run(debug=True)
